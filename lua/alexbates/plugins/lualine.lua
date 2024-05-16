92533f152d526a573ffa977e655f2749686f75f2
Alex Bates <>
2024-05-16 13:55:11 +0100

    feat: update lualine and add fidget

--- lua/alexbates/plugins/lualine.lua
+++ lua/alexbates/plugins/lualine.lua
@@ -2,8 +2,38 @@ return {
 	"nvim-lualine/lualine.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
+		local harpoon = require("harpoon.mark")
+		local function truncate_branch_name(branch)
+			if not branch or branch == "" then
+				return ""
+			end
+
+			-- Match the branch name to the specified format
+			local user, team, ticket_number = string.match(branch, "^(%w+)/(%w+)%-(%d+)")
+
+			-- If the branch name matches the format, display {user}/{team}-{ticket_number}, otherwise display the full branch name
+			if ticket_number then
+				return user .. "/" .. team .. "-" .. ticket_number
+			else
+				return branch
+			end
+		end
+		local function harpoon_component()
+			local total_marks = harpoon.get_length()
+			if total_marks == 0 then
+				return ""
+			end
+
+			local current_mark = "-"
+			local mark_idx = harpoon.get_current_index()
+			if mark_idx ~= nil then
+				current_mark = tostring(mark_idx)
+			end
+
+			return string.format("H:%s/%s", current_mark, total_marks)
+		end
+
 		local lualine = require("lualine")
-		local lazy_status = require("lazy.status") -- to configure lazy pending updates count
 
 		-- configure lualine with modified theme
 		lualine.setup({
@@ -11,15 +41,17 @@ return {
 				theme = "catppuccin",
 			},
 			sections = {
+				lualine_b = {
+					{ "branch", icon = "", fmt = truncate_branch_name },
+					harpoon_component,
+					"diff",
+					"diagnostics",
+				},
+				lualine_c = {
+					{ "filename", path = 1 },
+				},
 				lualine_x = {
-					{
-						lazy_status.updates,
-						cond = lazy_status.has_updates,
-						color = { fg = "#ff9e64" },
-					},
-					{ "encoding" },
-					{ "fileformat" },
-					{ "filetype" },
+					"filetype",
 				},
 			},
 		})
